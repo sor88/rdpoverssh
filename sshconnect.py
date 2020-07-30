a131b21d4d907a995bb17f458b81121d5869cd4a
Plus <plus@omsklug.com>
2020-07-30 18:10:58 +0500

    проброс порта ssh

--- sshconnect.py
+++ sshconnect.py
@@ -1,1 +1,16 @@
-import sshtunnel
+from sshtunnel import open_tunnel
+from time import sleep
+
+with open_tunnel(
+    ('90.150.80.113', 2222),
+    ssh_username="itu",
+    ssh_pkey="srv.key",
+    #ssh_password="pfxtvkbyerc",
+    remote_bind_address=('172.16.1.55', 3389),
+    local_bind_address=('localhost', 2222)
+) as server:
+
+    print(server.local_bind_port)
+    while True:
+        # press Ctrl-C for stopping
+        sleep(1)
