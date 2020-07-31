cee4b2abe0b74bcc037683a1bd54856c134de7bd
Plus <plus@omsklug.com>
2020-07-31 10:06:38 +0500

    добавлен поток проброса портов

--- remotecli2.1.py
+++ remotecli2.1.py
@@ -11,12 +11,15 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui.pushButton.clicked.connect(self.connectionstart)
         self.ui.pushButton_2.clicked.connect(self.vihod)
 
+
+
     def connectionstart(self):
        self.ui.label_4.setText("Подключение к серверу авторизации")
        self.ui.label.adjustSize()
        import sshconnect
-
-
+       import threading
+       tun1 = threading.Thread(target=sshconnect.sshtunconnect, daemon=True)
+       tun1.start()
 
     def vihod(self):
         """
--- sshconnect.py
+++ sshconnect.py
@@ -1,16 +1,18 @@
 from sshtunnel import open_tunnel
 from time import sleep
 
-with open_tunnel(
-    ('90.150.80.113', 2222),
-    ssh_username="itu",
-    ssh_pkey="srv.key",
-    #ssh_password="pfxtvkbyerc",
-    remote_bind_address=('172.16.1.55', 3389),
-    local_bind_address=('localhost', 2222)
-) as server:
 
-    print(server.local_bind_port)
-    while True:
-        # press Ctrl-C for stopping
-        sleep(1)
+def sshtunconnect():
+    with open_tunnel(
+        ('90.150.80.113', 2222),
+        ssh_username="itu",
+        ssh_pkey="srv.key",
+        #ssh_password="pfxtvkbyerc",
+        remote_bind_address=('172.16.1.55', 3389),
+        local_bind_address=('localhost', 2222)
+    ) as server:
+
+        print(server.local_bind_port)
+        while True:
+            # press Ctrl-C for stopping
+            sleep(1)
