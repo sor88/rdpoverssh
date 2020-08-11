ffb0ac297668bf7d05baafbd8fee6cc2ab4c373f
Plus <plus@omsklug.com>
2020-08-11 08:30:00 +0500

    установка placeholder

--- remotecli.py
+++ remotecli.py
@@ -12,8 +12,9 @@ class mywindow(QtWidgets.QMainWindow):
         super(mywindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.lineEdit.selectionChanged.connect (self.ui.lineEdit.clear)
-        self.ui.lineEdit_2.selectionChanged.connect (self.ui.lineEdit_2.clear)
+
+        self.ui.lineEdit.setPlaceholderText("Логин")
+        self.ui.lineEdit_2.setPlaceholderText ("Пароль")
         self.ui.pushButton.clicked.connect(self.connectionstart)
         self.ui.statusbar.showMessage("Программа готова к работе")
         self.ui.pushButton_2.clicked.connect(QCoreApplication.instance().quit)
